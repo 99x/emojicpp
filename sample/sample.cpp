ed3a2f81681506def6854541c439391e57bf9612
PH03be <phoebeleong8@gmail.com>
2021-05-23 16:50:08 +1000

    Update sample.cpp

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -2,6 +2,6 @@
 #include "emoji.h"
 using namespace std;
 int main() {
-    cout << emojicpp::emojize("Emoji :smile: for c++ :+1:") << endl;
+    cout << emojicpp::emojize("Hello World! :earth_africa:") << endl;
     return 0;
 }
